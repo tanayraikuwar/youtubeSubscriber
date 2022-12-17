e975b44eb30258d8ff8d0a171fddf1082217c8d9
tanayraikuwar <tanayraikuwar26@gmail.com>
2022-12-17 16:43:46 +0530

    some changes

--- src/createDatabase.js
+++ src/createDatabase.js
@@ -3,7 +3,7 @@ const subscriberModel = require('./models/subscribers')
 const data = require('./data')
 
 // Connect to DATABASE
-const DATABASE_URL = "mongodb://localhost/subscribers";
+const DATABASE_URL = "mongodb+srv://tanay:tanay@cluster0.pgziewj.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection
 db.on('error', (err) => console.log(err))
